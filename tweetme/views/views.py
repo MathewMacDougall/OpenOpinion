b78d76b811361afd8ff72190e979fe070bfd25ad
Mathew MacDougall <mmacd97@gmail.com>
2018-01-14 00:06:33 -0800

    separated functions and added support for multiple keywords

--- tweetme/views/views.py
+++ tweetme/views/views.py
@@ -1,5 +1,7 @@
 from django import http
 import twitter
+import numpy as np
+import random
 
 CONSUMER_KEY = "ET6mcjXi8L6RxK5kH2e4zDBCa"
 CONSUMER_SECRET = "AhlnBaCBtpxmoYbH4aefITQHYDiCP8Plo0ejqOVrc4NYQiqLDk"
@@ -10,22 +12,48 @@ auth = twitter.OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 tweety = twitter.Twitter(auth=auth)
 
 def analyze(request):
-    q = request.GET['user_query']
+    keywords = request.GET['keyword']
+    print("keywords are {} and type is {}".format(keywords, type))
 
+    # The array where we store the results for each keyword
+    result = []
+    sentiment_scores = []
+    weights = []
 
-    res = tweety.search.tweets(q=q,lang='en',result_type='recent',count=100)
+    for keyword in keywords: 
+        # Get the tweet data for each keyword
+        res = tweety.search.tweets(q=keyword,lang='en',result_type='recent',count=100)
 
-    entities = []
-    for s in res['statuses']:
-        text = ''.join([x for x in s['text'] if ord(x) < 256])
-        entities.append({
-            'text': text,
-            'retweet_count': s['retweet_count'],
-            'user_followers': s['user']['followers_count'],
-	    'created_at': s['created_at']
-        })
+        # Extract the important info from the tweets
+        tweets = []
+        for s in res['statuses']:
+            # Remove emojies from the text
+            text = ''.join([x for x in s['text'] if ord(x) < 256])
+            tweets.append({
+                'text': text,
+                'retweet_count': s['retweet_count'],
+                'user_followers': s['user']['followers_count'],
+                'created_at': s['created_at']
+            })
 
-    return http.JsonResponse(entities, safe=False)
+        # Get the sentiment score for this keyword
+        sentiment_scores.append(random.random())#get_sentiment_score(tweets))
+
+        # Get the weight for this keyword
+        weights.append(get_weight(tweets))
+
+    # Normalize the results before saving to the results
+    norm_sentiment = normalize(sentiment_scores)
+    norm_weights = normalize(weights)
+
+    for i in range(len(keywords)):
+        result.append({
+            "entity": keywords[i],
+            "weight": norm_weights[i],
+            "sentiment": norm_sentiment[i]
+            })
+
+    return http.JsonResponse(result, safe=False)
 
 def fakeanalyze(request):
     q = request.GET['user_query']
@@ -55,3 +83,21 @@ def fakeanalyze(request):
 
     return http.JsonResponse(fake, safe=False)
 
+# Returns the average sentiment for a list of tweets
+def get_sentiment_score(tweets): 
+    # Get the sentiment score for each tweet
+    # Use Kai's function call here
+    scores = [get_sentiment(tweet.text) for tweet in tweets]
+    mean_score = np.mean(scores)
+    return mean_score
+
+# Returns the weight for the keyword for a list of tweets
+def get_weight(tweets):
+   return 0.5 
+
+# Normalizes the values in an array
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
