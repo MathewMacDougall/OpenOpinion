a50bcdc933bf230c1ad0ade9a7ce7300f9e4c4e5
aidoraide <aidoraide@gmail.com>
2018-01-14 10:48:03 -0800

    Added multithreading

--- tweetme/views/views.py
+++ tweetme/views/views.py
@@ -9,9 +9,9 @@ CONSUMER_SECRET = "AhlnBaCBtpxmoYbH4aefITQHYDiCP8Plo0ejqOVrc4NYQiqLDk"
 ACCESS_TOKEN = "859835152507125761-M2zMka0P1zYdUbWZTdum3vnMI0IPnzO"
 ACCESS_SECRET = "2SyT2qaNGkYWZgXERiBX01lHZU3VnUgVSZWFRwVJ5p1zM"
 
-BATCHES_TWEETS = 1 # How many times to fetch tweets per keyword
+BATCHES_TWEETS = 2 # How many times to fetch tweets per keyword
 TOP_N = 10 # How many assicociated words to return
-COUNT = 50 # How many tweets to retrieve per query (MAX is 100)
+COUNT = 100 # How many tweets to retrieve per query (MAX is 100)
 SMALL_COUNT = 10 # How many tweets to retrieve per query (MAX is 100)
 
 auth = twitter.OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
