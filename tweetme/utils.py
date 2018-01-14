fdd2d04b51f290785d724ba88c5011a56f7a3aad
Kai Aldag <kaialdag@icloud.com>
2018-01-14 01:17:44 -0800

    save ur files

--- tweetme/utils.py
+++ tweetme/utils.py
@@ -20,7 +20,6 @@ def analyze_tweet(tweets):
             compounds.add(structure['object'])
         aggregate = ' '.join(compounds) # TODO: I've commited a grave sin.
 
-<<<<<<< HEAD
         entities = [word for word in tokens if word not in aggregate]
         entities.extend(list(compounds))
 
@@ -29,21 +28,3 @@ def analyze_tweet(tweets):
         tweet_metas.append({'sentiment': sent, 'entities': entities})
 
     return tweet_metas
-=======
-    return sentenceSentimentScore
-
-def get_top_n_entities(analyzed_tweets, n):
-
-
-# Returns the weight for the keyword for a list of tweets
-def get_weight(tweets):
-   return 0.5 
-
-# Normalizes the values in an array
-def normalize(v):
-    norm = np.linalg.norm(v)
-    if norm == 0: 
-       return v
-    return v / norm
-
->>>>>>> eff279b52aebbf384e35adf0d462a757e1d93a4a
